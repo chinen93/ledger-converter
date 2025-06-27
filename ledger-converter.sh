f5ff8539db2e9fd43b04de65d65b8f8d975d38e8
Pedro Chinen <chinen93@gmail.com>
2025-06-27 14:55:25 -0400

    Improve script to run ledger-converter

--- ledger-converter.sh
+++ ledger-converter.sh
@@ -1,9 +1,55 @@
 
-# Activate environment to run code
-source venv/bin/activate
+runLedgerConverter() {
+    # Activate environment to run code
+    source venv/bin/activate
 
-# Run code
-python3 ledger-converter.py
+    # Run code
+    python3 ledger-converter.py
 
-# Deactivate environment
-deactivate
+    # Deactivate environment
+    deactivate
+}
+
+runTests() {
+    # Activate environment to run code
+    source venv/bin/activate
+
+    # Run code
+    python3 -m unittest --verbose
+
+    # Deactivate environment
+    deactivate
+}
+
+printUsage() {
+    echo ""
+    echo "Ledger Converter"
+    echo ""
+    echo "Usage: $0 [ARGS]"
+    echo ""
+    echo "Arguments:"
+    echo "None        : Run Converter"
+    echo "-t | --test : Run Tests"
+    echo "-h | --help : Get Helper"
+    echo ""
+}
+
+# Handle arguments
+if [ -n "$1" ]; then
+    case "$1" in
+        "-t" | "--test")
+            runTests
+            ;;
+        "-h" | "--help")
+            printUsage
+            ;;
+        *)
+            echo "$0: illegal option $1"
+            printUsage
+            ;;
+    esac
+    exit
+fi
+
+# Default program without arguments
+runLedgerConverter
--- test-ledger-converter.sh
+++
@@ -1,8 +0,0 @@
-# Activate environment to run code
-source venv/bin/activate
-
-# Run code
-python3 -m unittest --verbose
-
-# Deactivate environment
-deactivate
